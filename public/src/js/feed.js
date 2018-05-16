bc284b3a34a8d5de2120be29ff0f95946aa7070f
collingss <nightharrier@gmail.com>
2018-05-16 12:12:04 +0200

    Service Workers - Advanced Caching

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -22,6 +22,16 @@ function openCreatePostModal() {
     // Because we used 'deferredPropmt, set it back to null
     deferredPrompt = null;
   }
+
+  // To remove a service worker
+  // if ('serviceWorker' in navigator) {
+  //   navigator.serviceWorker.getRegistrations()
+  //     .then(function (registrations) {
+  //       for (var i = 0; i < registrations.length; i++) {
+  //         registrations[i].unregister();
+  //       }
+  //     })
+  // }
 }
 
 function closeCreatePostModal() {
@@ -31,6 +41,28 @@ function closeCreatePostModal() {
 shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+
+// Currently not in use, allows to save assets in cache on demand otherwise
+function onSaveButtonClicked(event) {
+  console.log('clicked');
+  // Check to see if caching is supported
+  if ('caches' in window) {
+    // CACHE-ON-DEMAND
+    // Create user-created cache storage
+    caches.open('user-requested')
+      .then(function (cache) {
+        cache.add('https://httpbin.org/get');
+        cache.add('/src/images/sf-boat.jpg');
+      });
+  }
+}
+
+// Runs through the loop removing all child nodes untill all cleared
+function clearCards() {
+  while (sharedMomentsArea.hasChildNodes()) {
+    sharedMomentsArea.removeChild(sharedMomentsArea.lastChild);
+  }
+}
 
 // Added Shared moments card
 function createCard() {
@@ -51,15 +83,45 @@ function createCard() {
   cardSupportingText.className = 'mdl-card__supporting-text';
   cardSupportingText.textContent = 'In San Francisco';
   cardSupportingText.style.textAlign = 'center';
+  // var cardSaveButton = document.createElement('button');
+  // cardSaveButton.textContent = 'Save';
+  // cardSaveButton.addEventListener('click', onSaveButtonClicked);
+  // cardSupportingText.appendChild(cardSaveButton);
   cardWrapper.appendChild(cardSupportingText);
   componentHandler.upgradeElement(cardWrapper);
   sharedMomentsArea.appendChild(cardWrapper);
 }
 
-fetch('https://httpbin.org/get')
+
+var url = 'https://httpbin.org/get';
+// Set flag to see if network returned data before cache to prevenet overwriting
+var networkDataRecieved = false;
+
+// From Network
+fetch(url)
   .then(function (res) {
     return res.json();
   })
   .then(function (data) {
+    networkDataRecieved = true;
+    console.log('From web', data);
+    clearCards();
     createCard();
   });
+
+// From Cache
+if ('caches' in window) {
+  caches.match(url)
+    .then(function (response) {
+      if (response) {
+        return response.json();
+      }
+    })
+    .then(function (data) {
+      console.log('From cache', data);
+      if (!networkDataRecieved) {
+        clearCards();
+        createCard();
+      }
+    });
+};
--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,36 @@
-var CACHE_STATIC_NAME = 'static-v4';
+var CACHE_STATIC_NAME = 'static-v15';
 var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+var STATIC_FILES = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/src/js/app.js',
+  '/src/js/feed.js',
+  '/src/js/promise.js',
+  '/src/js/fetch.js',
+  '/src/js/material.min.js',
+  '/src/css/app.css',
+  '/src/css/feed.css',
+  '/src/images/main-image.jpg',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+];
+
+// Remove items from dynamic cache if too full
+// function trimCache(cacheName, maxItems) {
+//   caches.open(cacheName)
+//     .then(function (cache) {
+//       return cache.keys()
+//         .then(function (keys) {
+//           if (keys.length > maxItems) {
+//             cache.delete(keys[0])
+//               .then(trimCache(cacheName, maxItems));
+//           }
+//         })
+//     })
+// }
+
 
 // Triggered by browser
 self.addEventListener('install', function (event) {
@@ -13,24 +44,11 @@ self.addEventListener('install', function (event) {
       .then(function (cache) {
         console.log('[Service Worker] Precaching App Shell');
         // Think of it like caching 'urls'. Therefore need to cache / (i.e. home url)
-        cache.addAll([
-          '/',
-          '/index.html',
-          '/src/js/app.js',
-          '/src/js/feed.js',
-          '/src/js/promise.js',
-          '/src/js/fetch.js',
-          '/src/js/material.min.js',
-          '/src/css/app.css',
-          '/src/css/feed.css',
-          '/src/images/main-image.jpg',
-          'https://fonts.googleapis.com/css?family=Roboto:400,700',
-          'https://fonts.googleapis.com/icon?family=Material+Icons',
-          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-        ]);
+        cache.addAll(STATIC_FILES);
       })
   );
 });
+
 
 // Triggered by browser
 self.addEventListener('activate', function (event) {
@@ -54,40 +72,173 @@ self.addEventListener('activate', function (event) {
   return self.clients.claim();
 });
 
-// Triggered by web application / actual page
-self.addEventListener('fetch', function (event) {
-  //console.log('[Service Worker] Fetching something ...', event);
+//== Prevent false-positives (Q&A lecture 86)
+const isInCache = (requestURL, cacheArr) => cacheArr.some(url => url === requestURL.replace(self.origin, ''));
 
-  // Prevent Chrome error
-  if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
-    return;
+//== Alternate method
+function isInArray(string, array) {
+  var cachePath;
+  if (string.indexOf(self.origin) === 0) { // request targets domain where we serve the page from (i.e. NOT a CDN)
+    console.log('matched ', string);
+    cachePath = string.substring(self.origin.length); // take the part of the URL AFTER the domain (e.g. after localhost:8080)
+  } else {
+    cachePath = string; // store the full request (for CDNs)
   }
-  // Intercept request and return what is necessary
-  event.respondWith(
-    // Get cache value by request (i.e. by key)
-    caches.match(event.request)
-      .then(function (response) {
-        // If already in cache, return item else fetch it
-        if (response) {
-          return response;
-        } else {
-          // if nothing in the cache, return the original network request
-          return fetch(event.request)
-            // Pre-cache after any fetch event within application and return
-            .then(function (res) {
-              return caches.open(CACHE_DYNAMIC_NAME)
-                .then(function (cache) {
-                  // store clone of data in cache
-                  cache.put(event.request.url, res.clone());
-                  // return actual res which then gets consumed (i.e. used / removed thereafter)
-                  return res;
-                });
-            })
-            // Catch error when try to fetch from network and offline..
-            .catch(function (err) {
+  return array.indexOf(cachePath) > -1;
+}
 
+
+// Triggered by web application / actual page
+// #region Strategy: Cache-then-Network-&-Dynamic-Caching
+self.addEventListener('fetch', function (event) {
+  var url = 'https://httpbin.org/get';
+
+  // If request url is the same as 'url'
+  // Use cache-then-network strategy
+  if (event.request.url.indexOf(url) > -1) {
+    event.respondWith(
+      // Dynamic caching
+      caches.open(CACHE_DYNAMIC_NAME)
+        .then(function (cache) {
+          // Return the fetch request
+          return fetch(event.request)
+            // Intercept fetch and add it to the cache
+            .then(function (res) {
+              //trimCache(CACHE_DYNAMIC_NAME, 5);
+              cache.put(event.reqesut, res.clone());
+              // Return the actual response
+              return res;
             });
-        }
-      })
-  );
+        })
+    );
+  } else if (isInCache(event.request.url, STATIC_FILES)/*new RegExp('\\b' + STATIC_FILES.join('\\b|\\b') + '\\b').test(event.request.url)*/) {
+    // If the request url is contained within the STATIC_FILES array (ie app static files)
+    // Implement cache-only strategy
+    event.respondWith(
+      caches.match(event.request)
+    );
+  } else {
+    // For all urls that dont use cache-then-network strategy
+    // Use cache-with-network-fallback
+    event.respondWith(
+      caches.match(event.request)
+        .then(function (response) {
+          // If already in cache, return item else fetch it
+          if (response) {
+            return response;
+          } else {
+            // if nothing in the cache, return the original network request
+            return fetch(event.request)
+              // Pre-cache after any fetch event within application and return
+              .then(function (res) {
+                return caches.open(CACHE_DYNAMIC_NAME)
+                  .then(function (cache) {
+                    //trimCache(CACHE_DYNAMIC_NAME, 5);
+                    // store clone of data in cache
+                    cache.put(event.request.url, res.clone());
+                    // return actual res which then gets consumed (i.e. used / removed thereafter)
+                    return res;
+                  });
+              })
+              // Catch error when try to fetch from network and offline..
+              .catch(function (err) {
+                // If can't find a cached value, will return offline page
+                return caches.open(CACHE_STATIC_NAME)
+                  .then(function (cache) {
+                    // Return offline page only if trying to access /help page offline
+                    // [update]: if a request is an html page, show offline page
+                    if (event.request.headers.get('accept').includes('text/html')) {
+                      return cache.match('/offline.html');
+                    }
+                    // Can check for anthing else as well ie images, css and show fallback defaults etc
+                  });
+              });
+          }
+        }));
+  }
 });
+// #endregion Strategy: Cache-then-Network-&-Dynamic-Caching
+
+// #region: Other Strategies
+
+// #region Strategy: Cache-then-Network
+// self.addEventListener('fetch', function (event) {
+//   //console.log('[Service Worker] Fetching something ...', event);
+
+//   // Prevent Chrome error
+//   if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
+//     return;
+//   }
+//   // Intercept request and return what is necessary
+//   event.respondWith(
+//     // Get cache value by request (i.e. by key)
+//     caches.match(event.request)
+//       .then(function (response) {
+//         // If already in cache, return item else fetch it
+//         if (response) {
+//           return response;
+//         } else {
+//           // if nothing in the cache, return the original network request
+//           return fetch(event.request)
+//             // Pre-cache after any fetch event within application and return
+//             .then(function (res) {
+//               return caches.open(CACHE_DYNAMIC_NAME)
+//                 .then(function (cache) {
+//                   // store clone of data in cache
+//                   cache.put(event.request.url, res.clone());
+//                   // return actual res which then gets consumed (i.e. used / removed thereafter)
+//                   return res;
+//                 });
+//             })
+//             // Catch error when try to fetch from network and offline..
+//             .catch(function (err) {
+//               // If can't find a cached value, will return offline page
+//               return caches.open(CACHE_STATIC_NAME)
+//                 .then(function (cache) {
+//                   return cache.match('/offline.html');
+//                 });
+//             });
+//         }
+//       })
+//   );
+// });
+// #endregion Strategy: Cache then Network
+
+// #region Strategy: Cache-Only
+// self.addEventListener('fetch', function (event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//   );
+// });
+//#endregion Strategy: Cache-Only
+
+// #region Strategy: Network-Only
+// self.addEventListener('fetch', function (event) {
+//   event.respondWith(
+//     fetch(event.request)
+//   );
+// });
+//#endregion Strategy: Network-Only
+
+// #region Strategy: Network-1st-then-Cache-fallback
+// self.addEventListener('fetch', function (event) {
+//   event.respondWith(
+//     // Call fetch first, if successful -> will just work
+//     fetch(event.request)
+//       // Add dynamic caching if response works
+//       .then(function (res) {
+//         return caches.open(CACHE_DYNAMIC_NAME)
+//           .then(function (cache) {
+//             cache.put(event.request.url, res.clone());
+//             return res;
+//           });
+//       })
+//       //else catch failure and try to get from cache
+//       .catch(function (err) {
+//         return caches.match(event.request);
+//       })
+//   );
+// });
+//#endregion Strategy: Network 1st then Cache fallback
+
+// #endregion Other Strategies
